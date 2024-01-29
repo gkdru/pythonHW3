4e51854a0a2c180451bc19b578474498f418517d
Кудайберген Амир Маратулы <KudaybergenAm@lcoast.itstep>
2024-01-29 19:57:34 +0600

    initial commit

---
+++ dz.py
@@ -0,0 +1,22 @@
+# 1 задание
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+print(a[1::3])
+# 2 задание
+b = "Abracadabra"
+print(b.find("A"))
+# 3 задание
+print(b.startswith("a"))
+# 4 задание
+print(b.isdigit())
+#  5 задание
+c = ["Boom", "Lift", "Force", "Durian"]
+b = b + " " + " ".join(c)
+print(b)
+#  6 задание
+print(b.endswith("n"))
+#  7 задание
+d = "123123123password"
+print(d.isalnum())
+# 8 задание
+b = b.split()
+print(b[1])
